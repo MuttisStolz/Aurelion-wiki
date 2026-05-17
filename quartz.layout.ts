a62040c00f4f83915889441945ac25675002f044
MuttisStolz <115809408+MuttisStolz@users.noreply.github.com>
2026-05-17 13:30:14 +0200

    Quartz sync: May 17, 2026, 1:30 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+    filterFn: (node) => !node.displayName.startsWith("_"),
+  }),
   ],
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
